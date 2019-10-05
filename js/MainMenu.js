916c6cfe7afabc48280e2f76063e196d6e89312a
César Bolaños <cesar.bolanos.andino@gmail.com>
2019-10-05 11:54:31 -0600

    Attempt to style button with JS animations

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -1,27 +1,23 @@
 class MainMenu extends Phaser.Scene {
   constructor() {
     super({ key: 'mainmenu' });
+    window.GAME = this;
   }
 
   preload () {
-    this.load.css('landstar', 'assets/css/landstar.css');
+    this.load.css('landstar', 'assets/css/button.css');
     this.load.image('play-button', 'assets/button.png');
-
   }
 
   create() {
     // Set background color
     this.cameras.main.setBackgroundColor('#30336b');
-
-    // Add play button
-    this.load.image('play-button', 'assets/button.png');
-    const h1 = this.add.dom(400, 300, 'h1', null, 'Play');
-    h1.setClassName('play');
-
-    // const playButton = this.add.image(400, 300, 'play-button');
-    h1.setInteractive();
-    h1.once('pointerup', () => this.scene.start('bomber'), this);
-    // this.load.image('play-button', 'assets/button.png');
-
+    
+    const playButton = document.createElement('input');
+    playButton.className = 'bubbly-button';
+    playButton.type = 'button';
+    playButton.value = 'PLAY';
+    playButton.onclick = () => window.GAME.scene.start('bomber');
+    this.add.dom(400, 300, playButton).setInteractive(); 
   }
 }
